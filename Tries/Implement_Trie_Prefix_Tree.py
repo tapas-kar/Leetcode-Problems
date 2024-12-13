088218b00c5361fec677f6823c7e6ee5029d30af
Tapas Kar <tapask.kar123@gmail.com>
2024-12-13 00:48:46 -0500

    Implementing a Trie/Prefix Tree using the hashmap approach

---
+++ Tries/Implement_Trie_Prefix_Tree.py
@@ -0,0 +1,51 @@
+# Problem 208 A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. 
+# There are various applications of this data structure, such as autocomplete and spellchecker.
+
+
+# Implement the Trie class:
+
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+
+# Approach 1: This is the hashmap approach where the child node is a hashmap
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert (self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return True
