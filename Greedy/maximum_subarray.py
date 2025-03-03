bf990bffd0910ebc83ff7df7bdf14502b16d2b55
Tapas Kar <tapask.kar123@gmail.com>
2025-03-03 00:40:13 -0500

    Maximum subarray - Linear Runtime solution

---
+++ Greedy/maximum_subarray.py
@@ -0,0 +1,50 @@
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# Example 3:
+
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+ 
+
+# Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub  
+
+
+# def maxSubArray(self, nums: List[int]) -> int:
+#     max_ = -math.inf
+#     i_sum = 0
+            
+#     for right in range(len(nums)):
+#         i_sum = max(nums[right], nums[right] + i_sum)
+#         max_ = max(max_, i_sum)
+
+#     return max_
