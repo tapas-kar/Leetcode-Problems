ac190b6c2e702a82deea82f337226b6723bcf6f4
tapas_kar123 <tapask.kar123@gmail.com>
2025-09-16 20:45:20 -0400

    Removing unnecessary float conversion in division

--- Stack/Evaluate_Reverse_Polish_Notation.py
+++ Stack/Evaluate_Reverse_Polish_Notation.py
@@ -109,7 +109,7 @@ def evalRPN(tokens: List[str]) -> int:
             stack.append(stack.pop() * stack.pop())
         elif c == "/":
             a, b = stack.pop(), stack.pop()
-            stack.append(int(float(b) / a))
+            stack.append(int(b / a))
         else:
             stack.append(int(c))
     return stack[0]
