70330ef5deeb3e41170135f1f8f95eaeafae2fb8
tapas_kar123 <tapask.kar123@gmail.com>
2025-09-24 00:41:38 -0400

    Neetcode solution for finding target value

---
+++ Binary_Search/Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,59 @@
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 5000
+# -104 <= nums[i] <= 104
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -104 <= target <= 104
+
+# Approach 1: Neetcode solution
+
+def search(nums: list[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if target == nums[mid]:
+            return mid
+        
+        # left sorted portion
+        if nums[l] <= nums[mid]:
+            # because the sorted array has been rotated, the target could be either on the left or the right side
+            if target > nums[mid] or target < nums[l]:
+                # go right
+                l = mid + 1
+            else:
+                # go left
+                r = mid - 1
+        # nums[l] > nums[mid]
+        # that is why this is the right sorted portion
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return -1
