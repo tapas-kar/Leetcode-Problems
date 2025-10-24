34df82ad0c9401eb84dbc0472e42d10d2df5c4c5
tapas_kar123 <tapask.kar123@gmail.com>
2025-10-23 20:11:03 -0400

    Adding Count Good Nodes in Binary Tree DFS solution

---
+++ Trees/Count_Good_Nodes_In_Binary_Tree.py
@@ -0,0 +1,48 @@
+# Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+# Return the number of good nodes in the binary tree.
+
+# Example 1:
+# Input: root = [3,1,4,3,null,1,5]
+# Output: 4
+# Explanation: Nodes in blue are good.
+# Root Node (3) is always a good node.
+# Node 4 -> (3,4) is the maximum value in the path starting from the root.
+# Node 5 -> (3,4,5) is the maximum value in the path
+# Node 3 -> (3,1,3) is the maximum value in the path.
+
+
+# Example 2:
+# Input: root = [3,3,null,4,2]
+# Output: 3
+# Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+# Example 3:
+# Input: root = [1]
+# Output: 1
+# Explanation: Root is considered as good.
+ 
+
+# Constraints:
+
+# The number of nodes in the binary tree is in the range [1, 10^5].
+# Each node's value is between [-10^4, 10^4].
+
+# Runtime: O(n)
+# Space: O(log n) or in other words the height of the tree
+
+# Approach 1: Neetcode - Uses Recursive DFS Pre-order traversal (we will figure the reason)
+
+def goodNodes(self, root: TreeNode) -> int:
+
+    def dfs(node, maxVal):
+        if not node:
+            return 0
+        
+        res = 1 if node.val >= maxVal else 0
+        maxVal = max(maxVal, node.val)
+        res += dfs(node.left, maxVal)
+        res += dfs(node.right, maxVal)
+        return res
+    
+    return dfs(root, root.val)
