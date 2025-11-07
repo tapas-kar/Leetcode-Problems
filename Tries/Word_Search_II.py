f105c57ab502ea4ed4efb9fa659822b43ba20f9a
tapas_kar123 <tapask.kar123@gmail.com>
2025-11-07 00:48:11 -0500

    Adding Recursive DFS Backtracking with TrieNode solution for Word Search II

--- Tries/Word_Search_II.py
+++ Tries/Word_Search_II.py
@@ -24,4 +24,46 @@
 
 # Approach 1: Need to come back to this question after I finish Word Search I in Backtracking
 
-# def findWords(board: list[list[str]], words: list[str]) -> list[str]:
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+    def addWord(self, word):
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isWord = True
+
+def findWords(board: list[list[str]], words: list[str]) -> list[str]:
+
+    root = TrieNode()
+    for w in words:
+        root.addWord(w)
+
+    ROWS, COLS = len(board), len(board[0])
+    result, visit = set(), set()
+
+    def dfs(r, c, node, word):
+        if (r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visit or board[r][c] not in node.children):
+            return
+        visit.add((r, c))
+        node = node.children[board[r][c]]
+        word += board[r][c]
+        if node.isWord:
+            result.add(word)
+
+        dfs(r - 1, c, node, word)
+        dfs(r + 1, c, node, word)
+        dfs(r, c - 1, node, word)
+        dfs(r, c + 1, node, word)
+
+        visit.remove((r, c))
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            dfs(r, c, root, "")
+
+    return list(result)
